f5134d57d8d184a95eda4858f92599801a077c6a
hikkosha <hikky93@mail.ru>
2023-10-29 00:18:45 +0300

    modified:   public/index.html 	deleted:    src/App.css 	modified:   src/App.js 	deleted:    src/App.test.js 	modified:   src/index.css 	modified:   src/index.js 	deleted:    src/logo.svg 	deleted:    src/reportWebVitals.js 	deleted:    src/setupTests.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+
+const url = 'https://mdblist.p.rapidapi.com/?s=Avengers';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '723ad6ec05mshfb786ab3d93e6c5p18903djsn77e1fedf719a',
+		'X-RapidAPI-Host': 'mdblist.p.rapidapi.com'
+	}
+};
+
 
 function App() {
+  const [data, setData] = useState([])
+
+  useEffect(()=>{
+    fetch(url,options)
+      .then(res => res.json())
+      .then(res => setData(res))
+  }, [])
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
