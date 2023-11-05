e61e514b3dd942503e7c5749ba5efb2ea144cffa
hikkosha <hikky93@mail.ru>
2023-11-05 04:16:42 +0300

    new file:   src/assets/2.png 	new file:   src/assets/3.png 	new file:   src/assets/4.png 	new file:   src/assets/5.png 	new file:   src/assets/6.png 	new file:   src/assets/7.png 	new file:   src/assets/8.png 	new file:   src/assets/Rating Button.svg 	new file:   src/assets/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1.svg 	new file:   src/assets/add.svg 	new file:   src/assets/star.svg 	modified:   src/components/Foot.jsx 	modified:   src/components/Header.jsx 	modified:   src/index.css

--- src/components/Foot.jsx
+++ src/components/Foot.jsx
@@ -1,3 +1,13 @@
+import add from '../assets/add.svg';
+import BW from '../assets/Shang-Chi-and-the-Legend-of-the-Ten-Rings-Releases-New 1.svg';
+import ministar from '../assets/star.svg';
+import SC from '../assets/2.png';
+import LO from '../assets/3.png';
+import HI from '../assets/4.png';
+import MH from '../assets/5.png';
+import F from '../assets/6.png';
+import TB from '../assets/7.png';
+import TA from '../assets/8.png';
 
 
 function Foot() {
@@ -6,7 +16,102 @@ function Foot() {
             <div className="Foot__header">
                 <h1 className="Foot__movies">Movies</h1>
                 <div className="dg">
-                    
+                    <div className="cart">
+                        <div className='cart__up'>
+                            <img src={ministar} alt="#" />
+                            <p className="num">6.8</p>
+                        </div>
+                        <img className='BW' src={BW} alt="#" />
+                        <h1 className='cart__h1'>Black Window</h1>
+                        <div className='cart__foot'>
+                            <img src={add} alt="#" />
+                            <p className="add">Add to my list</p>
+                        </div>
+                    </div>
+                    <div className="cart">
+                        <div className='cart__up'>
+                            <img src={ministar} alt="#" />
+                            <p className="num">7.9</p>
+                        </div>
+                        <img className='BW' src={SC} alt="#" />
+                        <h1 className='cart__h1'>Shang Chi</h1>
+                        <div className='cart__foot'>
+                            <img src={add} alt="#" />
+                            <p className="add">Add to my list</p>
+                        </div>
+                    </div>
+                    <div className="cart">
+                        <div className='cart__up'>
+                            <img src={ministar} alt="#" />
+                            <p className="num">8.4</p>
+                        </div>
+                        <img className='BW' src={LO} alt="#" />
+                        <h1 className='cart__h1'>Loki</h1>
+                        <div className='cart__foot'>
+                            <img src={add} alt="#" />
+                            <p className="add">Add to my list</p>
+                        </div>
+                    </div>
+                    <div className="cart">
+                        <div className='cart__up'>
+                            <img src={ministar} alt="#" />
+                            <p className="num">8.3</p>
+                        </div>
+                        <img className='BW' src={HI} alt="#" />
+                        <h1 className='cart__h1'>How I Met Your Mother</h1>
+                        <div className='cart__foot'>
+                            <img src={add} alt="#" />
+                            <p className="add">Add to my list</p>
+                        </div>
+                    </div>
+                    <div className="cart">
+                        <div className='cart__up'>
+                            <img src={ministar} alt="#" />
+                            <p className="num">8.3</p>
+                        </div>
+                        <img className='BW' src={MH} alt="#" />
+                        <h1 className='cart__h1'>Money Heist</h1>
+                        <div className='cart__foot'>
+                            <img src={add} alt="#" />
+                            <p className="add">Add to my list</p>
+                        </div>
+                    </div>
+                    <div className="cart">
+                        <div className='cart__up'>
+                            <img src={ministar} alt="#" />
+                            <p className="num">8.8</p>
+                        </div>
+                        <img className='BW' src={F} alt="#" />
+                        <h1 className='cart__h1'>Friends</h1>
+                        <div className='cart__foot'>
+                            <img src={add} alt="#" />
+                            <p className="add">Add to my list</p>
+                        </div>
+                    </div>
+                    <div className="cart">
+                        <div className='cart__up'>
+                            <img src={ministar} alt="#" />
+                            <p className="num">8.1</p>
+                        </div>
+                        <img className='BW' src={TB} alt="#" />
+                        <h1 className='cart__h1'>The Big Bang Theory</h1>
+                        <div className='cart__foot'>
+                            <img src={add} alt="#" />
+                            <p className="add">Add to my list</p>
+                        </div>
+                    </div>
+                    <div className="cart">
+                        <div className='cart__up'>
+                            <img src={ministar} alt="#" />
+                            <p className="num">7</p>
+                        </div>
+                        <img className='BW' src={TA} alt="#" />
+                        <h1 className='cart__h1'>Two And a Half Men</h1>
+                        <div className='cart__foot'>
+                            <img src={add} alt="#" />
+                            <p className="add">Add to my list</p>
+                        </div>
+                    </div>
                 </div>
             </div>
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,13 +2,12 @@ import apna from '../assets/ApnaTV.svg';
 import notification from '../assets/Vector.svg';
 import search from '../assets/eva_search-fill.svg';
 
-
 function Header() {
     return ( 
         <div className="header">
             <div className='header__text'>
-                <a href='#'><h4 className="header__text__movies">Movies</h4></a>
-                <a href=""><h4 className="header__text__movies">Favorites</h4></a>
+                <a href='#'><h4 className="header__text__movies" id='mov'>Movies</h4></a>
+                <a href="#"><h4 className="header__text__movies">Favorites</h4></a>
             </div>
             <a href='/'><img src={apna} alt="error" /></a>
             <div className='header__text'>
